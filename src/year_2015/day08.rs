21319425454f2711f10437c6f749332e7277ece1
Cifko <gcifko@gmail.com>
2023-12-03 21:18:06 +0100

    2015/8

--- src/year_2015/day08.rs
+++ src/year_2015/day08.rs
@@ -2,9 +2,33 @@ use crate::common::get_lines;
 
 pub fn solve() {
     println!("Day 8 of 2015");
-    let line = get_lines();
+    let lines = get_lines();
     let mut s1 = 0;
     let mut s2 = 0;
+    for line in &lines {
+        let mut skip = 0;
+        let mut last_char = ' ';
+        s1 += 2;
+        s2 += 2;
+        for c in line.chars() {
+            match c {
+                '\\' => s2 += 1,
+                '"' => s2 += 1,
+                _ => (),
+            }
+            if last_char == '\\' {
+                match c {
+                    'x' => s1 += 3,
+                    '"' => s1 += 1,
+                    '\\' => s1 += 1,
+                    _ => (),
+                }
+                last_char = ' ';
+            } else {
+                last_char = c;
+            }
+        }
+    }
     println!("Part 1: {}", s1);
     println!("Part 2: {}", s2);
 }
