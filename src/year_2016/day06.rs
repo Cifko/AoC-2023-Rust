edfb2bc6788d0b253995d59b9a447658f867a521
Cifko <gcifko@gmail.com>
2023-12-10 18:03:33 +0100

    2016/6

--- src/year_2016/day06.rs
+++ src/year_2016/day06.rs
@@ -1,10 +1,24 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::helpers::get_lines;
 
 pub fn solve() {
     println!("Day 6 of 2016");
     let lines = get_lines();
-    let mut s1 = 0;
-    let mut s2 = 0;
+    let mut rep = vec![HashMap::new(); 8];
+    for line in &lines {
+        for (i, c) in line.chars().enumerate() {
+            *rep[i].entry(c).or_insert(0) += 1;
+        }
+    }
+    let s1 = rep
+        .iter()
+        .map(|rep| rep.iter().max_by_key(|&(_, v)| v).map(|(k, _)| k).unwrap())
+        .collect::<String>();
+    let s2 = rep
+        .iter()
+        .map(|rep| rep.iter().min_by_key(|&(_, v)| v).map(|(k, _)| k).unwrap())
+        .collect::<String>();
     println!("Part 1: {}", s1);
     println!("Part 2: {}", s2);
 }
